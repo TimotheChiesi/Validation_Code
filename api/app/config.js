bc71e528d2f74dee7cc7f71159fd217f160a9a2f
Timothe C <timothe.chiesi@etu.mines-ales.fr>
2025-01-14 11:25:19 +0100

    Add new config

--- api/app/config.js
+++ api/app/config.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/e2e'
+    mongodb: 'mongodb+srv://timothechiesi:y4WfLFoRVIZB3S81@clustervalidationcode.cjxhe.mongodb.net/e2e'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/e2e'
+    mongodb: 'mongodb+srv://timothechiesi:y4WfLFoRVIZB3S81@clustervalidationcode.cjxhe.mongodb.net/e2e'
   }
 }
